380cf0507c9f45eda87add6fd0b617301708dd4e
Lucas Sas Ortiz Campos <lucassas@outlook.com>
2024-07-24 23:28:51 -0300

    Parte 1

---
+++ Exercicio Worker/Entities/HourContract.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Worker.Entities
+{
+    class HourContract
+    {
+        public DateTime Data { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public int Hours { get; set; }
+
+        public HourContract() { }
+        public HourContract(DateTime data, double valuePerHour, int hours)
+        {
+            Data = data;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return ValuePerHour*Hours; 
+        }
+    }
+}
